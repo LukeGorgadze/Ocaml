51491cb0ef8ab2e15bd2e0f9df8d6c6a830fea4c
LukeGorgadze <just.luke.gorgadze@gmail.com>
2022-05-23 15:05:50 +0400

    Update

--- Artemis/Lagrange/Lagranges.ml
+++ Artemis/Lagrange/Lagranges.ml
@@ -1,9 +1,12 @@
-let lagrangeL l v = 
-  List.fold_left (+.) 0.0 ( List.map (fun (a,b) -> 
+let lagrange lss v = 
+  List.fold_left (fun o p -> o +. p) 0.0 ( List.map 
+  (fun (a,b) -> 
   b*.(List.fold_left ( *. ) 1.0 
-  (List.map (fun (x,y) -> if (x != a) then (v -. x)/.(a -. x) else 1.0) l))) l);;
+  (List.map (fun (x,y) -> 
+  if (x != a) then (v -. x)/.(a -. x) 
+  else 1.0) lss))) lss);;
 
-lagrangeL [100.,231.; 200.,12.; 300.,382.5] 0.3;;
+lagrange [100.,231.; 200.,12.; 300.,382.5] 0.3;;
 
 let lagrangeR lst vall = 
   List.fold_right (+.) (List.map(fun(x,y) ->
